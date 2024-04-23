c1c0f24e6d0f2682f3af25c4df34dcc63b979336
xichen <2676174548@qq.com>
2024-04-23 20:00:29 +0800

    在网页上运行代码的maven项目，可直接run

---
+++ codepart/src/test/java/org/example/codepart/CodepartApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.codepart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodepartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
